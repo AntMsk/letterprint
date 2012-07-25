586de36b98767307dffe1ee8848326d0423c4130
Dmitrij Chest <antpslv@gmail.com>
2012-07-26 02:30:14 +0400

    full commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -7,6 +9,12 @@ SampleApp::Application.routes.draw do
   
   get "pages/help"
 
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
